5cff79e5c0c5b7b58798f3848bb14579f5fe30ef
jhossuan <jhossuacamposd86@gmail.com>
2022-09-05 20:46:21 -0500

    update

---
+++ src/components/Projects.js
@@ -0,0 +1,29 @@
+import telery1 from '../assets/devTelery/devTelery1.png'
+import telery2 from '../assets/devTelery/devTelery2.png'
+
+import nodeIsp from '../assets/img_nodeIsp/nodeIsp.png'
+import nodeIsp1 from '../assets/img_nodeIsp/nodeIsp1.png'
+import nodeIsp2 from '../assets/img_nodeIsp/nodeIsp2.png'
+import nodeIsp3 from '../assets/img_nodeIsp/nodeIsp3.png'
+import nodeIsp4 from '../assets/img_nodeIsp/nodeIsp4.png'
+
+import roundone1 from '../assets/img_roundone/roundone1.png'
+import roundone2 from '../assets/img_roundone/roundone2.png'
+import roundone3 from '../assets/img_roundone/roundone3.png'
+import roundone4 from '../assets/img_roundone/roundone4.png'
+import roundone5 from '../assets/img_roundone/roundone5.png'
+import roundone6 from '../assets/img_roundone/roundone6.png'
+
+import teleryLanding1 from '../assets/img_telery/teleryLanding.png'
+import teleryLanding2 from '../assets/img_telery/teleryLandin2.png'
+import teleryLanding3 from '../assets/img_telery/teleryLandin3.png'
+import teleryLanding4 from '../assets/img_telery/teleryLanding4.png'
+import teleryLanding5 from '../assets/img_telery/teleryLanding5.png'
+import teleryLanding5_1 from '../assets/img_telery/teleryLandin5.png'
+
+export const projects = [
+    {title:'Telery App', description:'Application of finances and customer management.', type:'Web App', creationTime:'12 weeks', img:[{telery1, telery2}]},
+    {title:'Node ISP', description:'ISP in development for business control and internet services.', type:'Web App', creationTime:'12 weeks and counting', img:[{nodeIsp, nodeIsp1, nodeIsp2, nodeIsp3, nodeIsp4}]},
+    {title:'Round One Developers', description:'Landing page to promote software development services.', type:'Landing Page', creationTime:'8 hours', img:[{roundone1, roundone2, roundone3, roundone4, roundone5, roundone6}]},
+    {title:'Telery Networks', description:'Landing page to promote dedicated internet services.', type:'Landing Page', creationTime:'8 hours', img:[{roundone1, roundone2, roundone3, roundone4, roundone5, roundone6}]},
+]
--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,3 +1,4 @@
+import { projects } from '../components/Projects'
 
 const Projects = () => {
 
@@ -6,6 +7,17 @@ const Projects = () => {
       <div className="mb-5 text-center">
         <p className="text-principal">All my</p>
         <h1 className="text-100 uppercase">Projects</h1>
+      </div>
+      <div className='grid'>
+        {projects.map((project, index) => {
+          return (
+            <div className='col'>
+              <h1>{project.title}</h1>
+              <p>{project.description}</p>
+            </div>
+          )
+          })
+        }
       </div>
     </section>
   )
